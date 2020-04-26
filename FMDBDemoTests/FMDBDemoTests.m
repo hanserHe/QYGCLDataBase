78b199cdc7ca0bacccff03e6f23f961b563b589f
HePengFei <hepengfei@qiyi.com>
2020-04-26 09:31:14 +0800

    Initial Commit

---
+++ FMDBDemoTests/FMDBDemoTests.m
@@ -0,0 +1,37 @@
+//
+//  FMDBDemoTests.m
+//  FMDBDemoTests
+//
+//  Created by Hanser on 2020/4/26.
+//  Copyright © 2020 Hanser. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface FMDBDemoTests : XCTestCase
+
+@end
+
+@implementation FMDBDemoTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
